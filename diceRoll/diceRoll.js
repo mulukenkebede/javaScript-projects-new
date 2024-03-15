cf35ba4560660449630b87f503923985668a0246
mulukenkebede <mule.andisew@gmail.com>
2024-03-15 21:46:22 +0300

    pomodoro timer project

--- diceRoll/diceRoll.js
+++ diceRoll/diceRoll.js
@@ -1,28 +1,28 @@
 let diceClick = document.getElementById("btn");
-let diceArr = [1, 2, 3, 4, 5, 6];
+
 let total = 0;
 diceClick.addEventListener("click", () => {
-  let result = Math.floor(Math.random() * diceArr.length); //this will generate arandom number b/n 0 and 5
+  let result = Math.floor(Math.random() * 6) + 1; //this will generate arandom number b/n 0 and 5
   let placeHolder = document.getElementById("input"); //help to append the img element
   let imgElement = document.createElement("img");
 
   if (result === 1) {
-    imgElement.src = "./images/dice 1.JPG";
+    imgElement.src = "./image/dice 1.JPG"; // instead wecan use &#9856 (this is die with 1 dot face)
     total += 1;
   } else if (result === 2) {
-    imgElement.src = "./images/dice 2.JPG";
+    imgElement.src = "./image/dice 2.JPG"; // instead wecan use &#9857 (this is die with 1 dot face)
     total += 2;
   } else if (result === 3) {
-    imgElement.src = "./images/dice 3.JPG";
+    imgElement.src = "./image/dice 3.JPG"; //instead wecan use &#9858 (this is die with 1 dot face)
     total += 3;
   } else if (result === 4) {
-    imgElement.src = "./images/dice 4.JPG";
+    imgElement.src = "./image/dice 4.JPG";
     total += 4;
   } else if (result === 5) {
-    imgElement.src = "./images/dice 5.JPG";
+    imgElement.src = "./image/dice 5.JPG";
     total += 5;
   } else {
-    imgElement.src = "./images/dice 6.JPG";
+    imgElement.src = "./image/dice 6.JPG";
     total += 6;
   }
   // Clear the existing content of the placeHolder element other wise every time it appends a chiled it will not erase the previouse
